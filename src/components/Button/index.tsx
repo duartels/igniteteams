a0ebcb03062d292a44bd06112a5003d812408c4e
Luan Duarte <luansouza789@gmail.com>
2023-01-25 18:10:12 -0300

    button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { TouchableOpacityProps } from "react-native"
+import { ButtonTypeStylesProps, Container, Title } from "./styles"
+
+type Props = TouchableOpacityProps & {
+  title: string;
+  type?: ButtonTypeStylesProps;
+}
+
+export const Button = ({title, type = 'PRIMARY', ...rest}: Props) => {
+  return (
+    <Container
+      type={type}
+      {...rest}
+    >
+      <Title>{title}</Title>
+    </Container>
+  )
+}
